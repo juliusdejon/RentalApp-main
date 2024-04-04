c56957091fb59300e4fdca2cda3bea10f4064e7f
Leo <leocesar_alcordo@hotmail.com>
2024-04-04 01:04:17 -0400

    Profile Screen Updated

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,12 +1,77 @@
-import React from 'react'
-import {View, Text} from 'react-native'
-
-function Profile() {
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
+function ProfileScreen(props) {
+  const { onLogout, user } = props;
   return (
-    <View><Text>
-      Profile
-      </Text></View>
-  )
-}
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <View
+        style={{
+          flex: 1,
+          width: "100%",
+          backgroundColor: "#34495e",
+        }}
+      ></View>
 
-export default Profile
+      <View
+        style={{
+          flex: 3,
+          width: "100%",
+          alignItems: "center",
+          justifyContent: "flex-start",
+          paddingTop: 20,
+          bottom: 120,
+        }}
+      >
+        <Image
+          style={styles.imageStyle}
+          source={require("../assets/icon.png")}
+        />
+        <Text style={styles.welcomeText}>Renter</Text>
+        <Text>{user}</Text>
+        <TouchableOpacity style={styles.button} onPress={onLogout}>
+          <Text style={styles.buttonText}>Logout</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+export default ProfileScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  welcomeText: {
+    fontSize: 27,
+    fontWeight: "bold",
+    marginVertical: 10,
+  },
+
+  button: {
+    padding: 10,
+    backgroundColor: "red",
+    borderRadius: 10,
+    alignItems: "center",
+    marginVertical: 10,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  imageStyle: {
+    width: 200,
+    height: 200,
+    borderColor: "black",
+    borderRadius: 100,
+    backgroundColor: "#97E7E1",
+    marginVertical: 10,
+    borderColor: "black",
+    borderWidth: 0.5,
+  },
+});
